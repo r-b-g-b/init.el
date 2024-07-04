d11a1c9d35c1772f25f92b43f36162eea660b4c6
Robert Gibboni <robert@drivendata.org>
2024-07-03 20:27:12 -0400

    vertico, consult, embark, marginalia

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; package --- Summary
+;;; package --- Summary
 ;;; Commentary:
 ;;; Code:
 
@@ -23,7 +23,9 @@
 (add-to-list 'load-path "~/.emacs.d/src")
 (add-to-list 'exec-path "~/anaconda3/bin" t)
 (add-to-list 'exec-path "~/.rbenv/shims" t)
-
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
 (set-face-attribute 'default nil :height 86)
 (setq-default flycheck-disabled-checkers '(python-pylint))
 (setq-default electric-indent-inhibit t)
@@ -48,7 +50,17 @@
 
 (use-package straight
   :custom
-  (straight-use-package-by-default t))
+  (straight-use-package-by-default t)
+  :init
+  (straight-use-package '( vertico :files (:defaults "extensions/*")
+                           :includes (vertico-buffer
+                                      vertico-directory
+                                      vertico-flat
+                                      vertico-indexed
+                                      vertico-mouse
+                                      vertico-quick
+                                      vertico-repeat
+                                      vertico-reverse))))
 
 (use-package dired
   :straight nil
@@ -159,22 +171,251 @@
 
 (use-package pager)
 
-(use-package counsel
-  :after (ivy org)
-  :config (counsel-mode)
-  :bind (:map org-mode-map ("C-c C-j" . counsel-outline)))
+(use-package vertico
+  :demand
+  :config
+  (setq vertico-cycle t)
+  ;; currently requires melpa version of vertico
+  (setq vertico-preselect 'directory)
+  :init
+  (vertico-mode)
+  (defun my/vertico-insert ()
+    (interactive)
+    (let* ((mb (minibuffer-contents-no-properties))
+           (lc (if (string= mb "") mb (substring mb -1))))
+      (cond ((string-match-p "^[/~:]" lc) (self-insert-command 1 ?/))
+            ((file-directory-p (vertico--candidate)) (vertico-insert))
+            (t (self-insert-command 1 ?/)))))
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
 
-(use-package ivy
-  :defer 0.1
-  :diminish
-  :bind (("C-c C-r" . ivy-resume)
-         ("C-x B" . ivy-switch-buffer-other-window))
+  ;; Show more candidates
+  ;; (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  (setq vertico-resize nil)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
+
+  :bind (:map vertico-map
+              ("/" . #'my/vertico-insert)))
+
+;; Configure directory extension.
+(use-package vertico-directory
+  :straight nil
+  :after vertico
+  :demand
+  ;; More convenient directory navigation commands
+  :bind (:map vertico-map
+              ("RET"   . vertico-directory-enter)
+              ("DEL"   . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+  ;; Tidy shadowed file names
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+
+;; Example configuration for Consult
+(use-package consult
+  ;; Replace bindings. Lazily loaded due by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("C-c C-j" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("C-s" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  ;; By default `consult-project-function' uses `project-root' from project.el.
+  ;; Optionally configure a different project root function.
+  ;;;; 1. project.el (the default)
+  ;; (setq consult-project-function #'consult--default-project--function)
+  ;;;; 2. vc.el (vc-root-dir)
+  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
+  ;;;; 3. locate-dominating-file
+  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
+  ;;;; 4. projectile.el (projectile-project-root)
+  (autoload 'projectile-project-root "projectile")
+  (setq consult-project-function (lambda (_) (projectile-project-root)))
+  ;;;; 5. No project support
+  ;; (setq consult-project-function nil)
+
+  ;; virtual buffers
+  ;; b Buffers
+  ;; SPC Hidden buffers
+  ;; * Modified buffers
+  ;; f Files (Requires recentf-mode)
+  ;; r File registers
+  ;; m Bookmarks
+  ;; p Project
+  ;; Custom other sources configured in consult-buffer-sources.
+
+)
+
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode))
+
+(use-package embark
+  :ensure t
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc. You may adjust the
+  ;; Eldoc strategy, if you want to see the documentation from
+  ;; multiple providers. Beware that using this can be a little
+  ;; jarring since the message shown in the minibuffer can be more
+  ;; than one line, causing the modeline to move up and down:
+
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+(use-package orderless
   :custom
-  (ivy-count-format "(%d/%d) ")
-  (ivy-use-virtual-buffers t)
-  :config (ivy-mode))
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
 
-(use-package ivy-avy)
+(use-package wgrep
+  :bind ( :map grep-mode-map
+          ("e" . wgrep-change-to-wgrep-mode)
+          ("C-x C-q" . wgrep-change-to-wgrep-mode)
+          ("C-c C-c" . wgrep-finish-edit)))
+
+(use-package projectile
+  :diminish projectile-mode
+  ;; :custom ((projectile-completion-system 'ivy))
+  :config (projectile-mode)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/code")
+    (setq projectile-project-search-path '("~/code")))
+  (setq projectile-switch-project-action #'projectile-dired))
 
 (use-package avy
   :bind ("M-j" . avy-goto-char-timer)
@@ -250,18 +491,6 @@
   (undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
   :bind (("C-x u" . my/undo-tree-visualize)))
 
-(use-package ivy-hydra
-  :after hydra)
-
-(use-package ivy-rich
-  :after ivy
-  :custom
-  (ivy-virtual-abbreviate 'full
-                          ivy-rich-switch-buffer-align-virtual-buffer t
-                          ivy-rich-path-style 'abbrev)
-  :config
-  (ivy-rich-mode))
-
 (setq browse-url-browser-function 'browse-url-firefox)
 (setq browse-url-secondary-browser-function 'eww-browse-url)
 
@@ -281,11 +510,6 @@
         (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
         (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
         (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
-
-(use-package swiper
-  :after ivy
-  :bind (("C-s" . swiper)
-         ("C-r" . swiper)))
 
 (use-package all-the-icons)
 
@@ -325,6 +549,7 @@
   :hook (prog-mode . nlinum-mode))
 
 (use-package editorconfig)
+
 (use-package jsonrpc)
 
 (use-package copilot
@@ -357,7 +582,10 @@
      ("u" outline-up-heading "Up level")
      ("f" org-forward-heading-same-level "Forward same level")
      ("l" org-forward-heading-same-level "Forward same level")
-     ("h" org-backward-heading-same-level "Backward same level")
+     ("h" org-backward-heading-same-level "Backward same level"))
+    "Focus"
+    (("M-s" org-narrow-to-subtree "Narrow to subtree")
+     ("M-w" widen "Widen")
      ("/" org-tree-slide-mode :toggle t)
      ("," org-tree-slide-move-previous-tree "Previous tree")
      ("." org-tree-slide-move-next-tree "Next tree"))
@@ -370,9 +598,9 @@
      ("$" org-archive-subtree "Archive")
      ("d" org-cut-subtree "Kill")
      ("a" org-insert-heading "Insert above")
-     ("b" (org-insert-heading t)"Insert below"))
+     ("b" (org-insert-heading t) "Insert below"))
     "Act"
-    (("s" counsel-outline "Search" :color red :column "Actions")
+    (("s" consult-outline "Search" :color red :column "Actions")
      ("w" org-copy-subtree "Copy")
      ("y" org-paste-subtree "Paste")
      ("I" org-clock-in "Clock in")
@@ -512,8 +740,6 @@
   (org-roam-db-autosync-mode)
   :commands (org-roam-node-list))
 
-(use-package ivy-bibtex)
-
 (use-package org-msg
   :after org
   :config
@@ -621,16 +847,6 @@ Robert
   :custom
   (org-pandoc-options-for-gfm '((wrap . "none"))))
 
-(use-package deft
-  :after org
-  :bind
-  ("C-c n d" . deft)
-  :custom
-  (deft-recursive t)
-  (deft-use-filter-string-for-filename t)
-  (deft-default-extension "org")
-  (deft-directory org-roam-directory))
-
 (use-package gptel
   :custom
   (gptel-api-key (plist-get (nth 0 (auth-source-search :max 1 :host "openai.com")) :secret))
@@ -641,9 +857,9 @@ Robert
   :init
   (require 'llm-ollama)
   (setopt ellama-provider
-		    (make-llm-ollama
-		     :chat-model "llama3:8b-instruct-q4_0"
-		     :embedding-model "llama3:8b-instruct-q4_K_M"))
+                    (make-llm-ollama
+                     :chat-model "llama3:8b-instruct-q4_0"
+                     :embedding-model "llama3:8b-instruct-q4_K_M"))
   :custom
   (ellama-keymap-prefix "C-c l")
   (ellama-user-nick (getenv "USER"))
@@ -681,22 +897,6 @@ Robert
   (add-to-list
    'proced-format-alist
    '(custom user pid ppid sess tree pcpu pmem rss start time state (args comm))))
-
-(use-package projectile
-  :diminish projectile-mode
-  :custom ((projectile-completion-system 'ivy))
-  :config (projectile-mode)
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  ;; NOTE: Set this to the folder where you keep your Git repos!
-  (when (file-directory-p "~/code")
-    (setq projectile-project-search-path '("~/code")))
-  (setq projectile-switch-project-action #'projectile-dired))
-
-(use-package counsel-projectile
-  :after (counsel ivy projectile)
-  :config (counsel-projectile-mode))
 
 (use-package popper
   :bind (("C-`" . popper-toggle)
@@ -841,7 +1041,7 @@ Robert
   (defhydra python:hydra (python-mode-map "C-c h" :color pink)
     ("n" end-of-defun "Next" :column "Navigate")
     ("p" beginning-of-defun "Previous")
-    ("s" counsel-imenu "Search")
+    ("s" consult-imenu "Search")
     ("q" nil "Quit" :color blue :column "Quit")))
 
 (use-package pyvenv
@@ -942,7 +1142,9 @@ Robert
 
 (use-package yaml-mode
   :config
-  :bind (:map yaml-mode-map ("C-c C-j" . counsel-imenu)))
+  :bind (:map yaml-mode-map ("C-c C-j" . consult-imenu)))
+
+(use-package pandoc-mode)
 
 (use-package scad-mode
   :custom
